61cd26ecca8a1ab799ca7171d15658c06b938aa5
Shafath <shafath061@gmail.com>
2026-04-09 11:02:54 +0600

    Initial commit

--- app.js
+++ app.js
@@ -88,6 +88,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Page not found' });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', ()=> {
   console.log(`Server running on http://localhost:${port}`);
 });
